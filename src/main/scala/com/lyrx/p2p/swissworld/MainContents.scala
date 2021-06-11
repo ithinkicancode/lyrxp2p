95666095d0ebdf9b48b3e96e087230f8b45f68ed
alex <enna@mailbox.org>
2021-06-11 15:07:11 +0200

    Revert "Introduce react-router to sbt"
    This reverts commit 79087f7aeb2831dbf6124e086ff7192fb2cd2fb7.

--- src/main/scala/com/lyrx/p2p/swissworld/MainContents.scala
+++ src/main/scala/com/lyrx/p2p/swissworld/MainContents.scala
@@ -1,12 +1,9 @@
 package com.lyrx.p2p.swissworld
 
 import com.lyrx.p2p.{AppProps, AppState}
+import slinky.core.Component
 import slinky.core.annotations.react
 import slinky.core.facade.ReactElement
-import slinky.core.{Component, FunctionalComponent}
-import vision.id.rrd.facade.history.mod.{Action, History, Location}
-import vision.id.rrd.facade.reactRouterDom.{mod => rreact}
-
 
 @react class MainContents extends Component {
 
@@ -15,39 +12,6 @@ import vision.id.rrd.facade.reactRouterDom.{mod => rreact}
 
   def initialState = AppState("")
 
-  def backButton() = {
-
-
-    val h: History[Nothing] = rreact.useHistory()
-
-
-    h.listen((e1, e2) => {
-      val location: Location[Nothing] = e1;
-      val action: Action = e2;
-
-      println(e2)
-
-
-    })
-  }
-
-  override def componentDidMount(): Unit = {
-    super.componentDidMount()
-    //backButton()
-  }
-
-  def cc() = FunctionalComponent[Props] { props => {
-    ComponentConfiguration.getComponent(props)
-  }
-
-  }
-
-  def render(): ReactElement = {
-
-
-    cc()(props)
-
-
-  }
+  def render(): ReactElement = ComponentConfiguration.getComponent(props)
 
 }
